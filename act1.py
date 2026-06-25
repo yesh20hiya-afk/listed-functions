e14dbde3ab5408f3455faac35180fcec3adb4c8e
yesh20hiya-afk <yesh20hiya@gmail.com>
2026-06-24 21:22:32 -0400

    Add files via upload

---
+++ act1.py
@@ -0,0 +1,13 @@
+empty_list= []
+print()
+
+numbers = [1,2,3,4,5,]
+print(numbers)
+
+tripples = [1,2,3]*3
+print(tripples)
+
+alist = [100,200,300,400,500]
+alist = alist[::-1]
+
+print(alist, "\n")
